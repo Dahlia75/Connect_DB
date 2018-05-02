3b2e0fcbfc2535410ee948e06cb5ea00462db49b
Dahlia75 <d.aljabari75@gmail.com>
2018-05-01 17:04:38 -0700

    Connect to DB

---
+++ lookup_people.js
@@ -0,0 +1,27 @@
+const pg = require("pg");
+
+var argv = process.argv;
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  console.log(argv[2]);
+  client.query("SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1", [argv[2]], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows);
+    client.end();
+  });
+});
